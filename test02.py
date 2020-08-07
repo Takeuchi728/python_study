afa7ab81299dab8b7eb5c5659c7c8d2855b8a57d
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-08-07 13:02:05 +0900

    add new file

---
+++ test02.py
@@ -0,0 +1,11 @@
+print('モジュールのロード')
+
+def test():
+    print('関数:testを呼び出しました')
+# pythonは【】を用いないブロック構造
+# test関数は3～4行
+if __name__ == '__main__':
+
+    print('Python_izm')
+    # print('パイソンイズム')
+    test()
