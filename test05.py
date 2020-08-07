4b49674517d16b1ecea74952134e7e65aae3f54d
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-08-07 19:30:35 +0900

    add new file

---
+++ test05.py
@@ -0,0 +1,27 @@
+x = 100
+
+print(x + 10)
+print(x - 10)
+print(x * 10)
+print(x / 10)
+
+# 数値へ変換
+str = '123'
+
+print(int(str) + 100)
+
+# 浮動小数点数
+x = .5
+
+print(x)
+
+str = '10.5'
+
+print(float(str) + 10)
+
+#複素数
+complex = 10 + 5j
+
+print(complex)
+print(complex.real)
+print(complex.imag)
