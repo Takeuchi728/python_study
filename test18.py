5047f2f3d768e8662b44c6843f96fd62073dfbe9
takeuchi728 <takeuchi@pl.info-kochi-tech.ac.jp>
2020-08-09 13:08:19 +0900

    add new file

---
+++ test18.py
@@ -0,0 +1,21 @@
+for_sample = []
+for_sample.append('python')
+for_sample.append('-')
+for_sample.append('izm')
+for_sample.append('for')
+for_sample.append('statement')
+for_sample.append('sample')
+
+for value in for_sample:
+    print(value)
+
+for value in 'ABCDEF':
+    print('value')
+
+list1 = [['https', 'www'], ['python', 'com']]
+
+for value in list1:
+    print(value)
+
+for value1, value2 in list1:
+    print(value1, value2)
